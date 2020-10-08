e3736d77178e0b028ade6fdf6592ec093bbebbcf
Goncalo Ribeiro <goncalosribeiro@gmail.com>
2020-10-08 21:39:35 +0100

    Product update

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -1,5 +1,6 @@
 const Product = require('../database/models/product');
 const formidable = require('formidable');
+const _ = require('lodash');
 const fs = require('fs');
 
 exports.productById = async (req, res, next, id) => {
@@ -19,6 +20,7 @@ exports.productById = async (req, res, next, id) => {
 
 exports.create = (req, res) => {
   let form = new formidable.IncomingForm();
+  form.keepExtensions = true;
   form.parse(req, (err, fields, files) => {
     if (err) {
       return res.status(400).json({ error: 'Image could not be uploaded' })
@@ -57,5 +59,36 @@ exports.remove = (req, res) => {
       return res.status(400).json(err)
     }
     res.json({ message: 'Product deleted successfully' })
+  })
+}
+
+exports.update = (req, res) => {
+  let form = new formidable.IncomingForm();
+  form.keepExtensions = true;
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      return res.status(400).json({ error: 'Image could not be uploaded' })
+    }
+    const { name, description, price, category, quantity, shipping } = fields;
+    if (!name || !description || !price || !category || !quantity || !shipping) {
+      return res.status(400).json({ error: 'All fields are required' })
+    }
+
+    let product = req.product;
+    product = _.extend(product, fields);
+
+    if (files.photo) {
+      if (files.photo.size > 1000000) {
+        return res.status(400).json({ error: 'Image must be less than 1Mb size' })
+      }
+      product.photo.data = fs.readFileSync(files.photo.path)
+      product.photo.contentType = files.photo.type
+    }
+    product.save((err, result) => {
+      if (err) {
+        return res.status(400).json(err)
+      }
+      res.json(result)
+    })
   })
 }
